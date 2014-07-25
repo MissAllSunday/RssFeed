a05f67c87e16f99c7c9ec4e5a5b107e76a84e6b2
Suki <suki@missallsunday.com>
2014-07-25 10:20:45 -0500

    First version of RssFeed with the usual Ohara stuff
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/RssFeed.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package RssFeed mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2014, Jessica González
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+// Use composer!
+require_once ($boarddir .'/vendor/autoload.php');
+
+class RssFeed extends Suki\Ohara
+{
+	public $name = __CLASS__;
+
+	public function __construct()
+	{
+		$this->setRegistry();
+	}
+}
