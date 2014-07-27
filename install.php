780a1ac1aec029b0c391c175c4ec8afd03f2542d
Suki <suki@missallsunday.com>
2014-07-27 11:23:19 -0500

    hooks and install files
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ install.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package RssFeed mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2014, Jessica González
+ * @license http://www.mozilla.org/MPL/2.0/
+ */
+
+	if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+		require_once(dirname(__FILE__) . '/SSI.php');
+	elseif (!defined('SMF'))
+		exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
